139abe84bddfecefccfb95166ae99907859c978e
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:11:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "\n\tHammad ali\t\n";
+console.log(personName);
+let personName2 = personName.trim();
+console.log(personName2);
---
+++ main.ts
@@ -0,0 +1,8 @@
+let personName  :string = "\n\tHammad ali\t\n";
+
+console.log(personName);
+
+
+let personName2 = personName.trim();
+console.log(personName2);
+
